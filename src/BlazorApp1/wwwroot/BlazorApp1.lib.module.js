baef24e1b5f7c9b1ccad084c7f8f457a2c022d28
Kebechet <samosidor@gmail.com>
2024-04-06 15:01:42 +0200

    added same initializer for Blazor where it works

---
+++ src/BlazorApp1/wwwroot/BlazorApp1.lib.module.js
@@ -0,0 +1,15 @@
+﻿export function beforeStart(options, extensions) {
+    console.log("beforeStart");
+
+    var element = document.createElement('script');
+    element.src = "/test1.js";
+    document.head.appendChild(element);
+}
+
+export function afterStarted(blazor) {
+    console.log("afterStarted");
+
+    var element = document.createElement('script');
+    element.src = "/test2.js";
+    document.head.appendChild(element);
+}
